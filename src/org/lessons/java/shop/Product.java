417071696b83cd5f0b57316430b49f9d4d6d5b7c
vmqxdv <vquintilio@gmail.com>
2025-07-02 13:04:37 +0200

    feat: implement Product class with attributes and methods

--- src/org/lessons/java/shop/Product.java
+++ src/org/lessons/java/shop/Product.java
@@ -1,5 +1,7 @@
 package org.lessons.java.shop;
 
+import java.util.Random;
+
 public class Product {
 
   int code;
@@ -7,5 +9,28 @@ public class Product {
   String description;
   float price;
   float iva;
+
+  public Product(String name, String description, float price, float iva) {
+
+    Random random = new Random();
+
+    this.code = random.nextInt(8);
+    this.name = name;
+    this.description = description;
+    this.price = price;
+    this.iva = iva;
+  }
+
+  public float getPriceBeforeTax() {
+    return this.price;
+  }
+
+  public float getPriceAfterTax() {
+    return (this.price * this.iva);
+  }
+
+  public String getFullName() {
+    return this.name + "-" + this.code;
+  }
 
 }
